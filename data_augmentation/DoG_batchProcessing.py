fe93d05308e1a0e889ac35a0f9a7f419d16aa5b7
Zac Swider <51267747+zacswider@users.noreply.github.com>
2022-08-09 09:30:47 -0500

    Switched from linear to multiprocessing of DoG filter

--- data_augmentation/DoG_batchProcessing.py
+++ data_augmentation/DoG_batchProcessing.py
@@ -2,22 +2,24 @@ import os
 from tqdm import tqdm
 from skimage import filters
 from tifffile import imread, imwrite
+import multiprocess as mp
 
 def main():
-    processing_dir = '/Volumes/bigData/wholeMount_volDist/220712_Fix_Emb_Flvw_Chn1GAP_PI_aTub647_Processed/N2V_Denoised/16bit_scaleZ'
+    processing_dir = '/Volumes/bigData/wholeMount_volDist/220726-0805_Fix_Emb_Flvw_Chn1GAP_PI_aTub647_Processed/0_N2V_Denoised/16bit_scaleZ'
     save_dir = os.path.join(processing_dir, 'sbdl2_processed')
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
     im_names = [f for f in os.listdir(processing_dir) if f.endswith('.tif') and not f.startswith('.')]
 
-    with tqdm(total=len(im_names)) as pbar:
-        for im_name in im_names:
-            im_base = im_name.split('.')[0]
-            im = imread(os.path.join(processing_dir, im_name))
-            sbdl2 = filters.difference_of_gaussians(im, low_sigma=2, high_sigma=128)
-            imwrite(os.path.join(save_dir, f'{im_base}_sbdl2.tif'), sbdl2)
-            pbar.update(1)
+    def blur_image(im_name):
+        im_base = im_name.split('.')[0]
+        im = imread(os.path.join(processing_dir, im_name))
+        sbdl2 = filters.difference_of_gaussians(im, low_sigma=2, high_sigma=128)
+        imwrite(os.path.join(save_dir, f'{im_base}_sbdl2.tif'), sbdl2)
+    
+    with mp.Pool() as pool:
+        r = list(tqdm(pool.imap(blur_image, im_names), total=len(im_names)))
 
 if __name__ == '__main__':
     main()
