b3b2dac38e106c1cab568c9ee19395b4a9227013
Zac Swider <51267747+zacswider@users.noreply.github.com>
2022-08-24 12:42:32 -0500

    Update process_embs.py

--- process_embs.py
+++ process_embs.py
@@ -3,6 +3,16 @@ import os
 import multiprocess as mp
 import time
 
+'''
+This script iterates through a folder expecting each subdirectory to contain a set of images:
+1) a cellpose segmentation file (.npy)
+2) propidium iodide staining file (.tif)
+3) anti-tubulin staining file (.tif)
+4) DoG-filtered anti-tubulin staining file (.tif)
+
+This script will then process each embryo in parallel and save the results.
+'''
+
 if __name__ == '__main__':
     maindir = '/Volumes/bigData/wholeMount_volDist/220805-0712_Combined'
     subdirs = [os.path.join(maindir, d) for d in os.listdir(maindir) if os.path.isdir(os.path.join(maindir, d))]
